956046ce2e25426d30a64996dde6e51d6fd23444
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-04-11 09:38:02 -0600

    Add random quote route

--- public/script.js
+++ public/script.js
@@ -1,9 +1,44 @@
-fetch("http://localhost:8080/prueba")
-    .then(res => {
-        if(res.ok) {
-            res.text()
-                .then(response => {
-                    console.log(response);
-                });
-        }
-    });
+const fetchRandomButton = document.getElementById('fetch-random');
+const fetchAllQuotes = document.getElementById('fetch-all');
+const fetchByAuthor = document.getElementById('fetch-by-author');
+
+const quoteContainer = document.getElementById('quote-container');
+
+const resetQuotes = () => {
+    quoteContainer.innerHTML = '';
+};
+
+const renderError = response => {
+    quoteContainer.innerHTML = `<p>Your request returned an error:</p>
+    <p>Code: ${response.status}</p>
+    <p>${response.statusText}</p>`;
+};
+
+const renderQuotes = (quotes = []) => {
+    resetQuotes();
+    if (quotes.length > 0) {
+        quotes.forEach(quote => {
+            const newQuote = document.createElement('div');
+            newQuote.className = 'single-quote';
+            newQuote.innerHTML = `<div class="quote-text">${quote.quote}</div>
+            <div class="attribution">- ${quote.person}</div>`;
+            quoteContainer.appendChild(newQuote);
+        });
+    } else {
+        quoteContainer.innerHTML = '<p>Your request returned no quotes.</p>';
+    }
+};
+
+fetchRandomButton.addEventListener('click', () => {
+    fetch('http://localhost:8080/api/quotes/random')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                renderError(response);
+            }
+        })
+        .then(response => {
+            renderQuotes([response.quote]);
+        });
+});
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+app.use(express.static('public'));
 app.use(cors());
 
-app.get("/prueba", (req, res) => {
-    res.send("Hello!");
+app.get('/api/quotes/random', (req, res) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({ quote: randomQuote });
 });
 
 app.listen(8080, () => {
